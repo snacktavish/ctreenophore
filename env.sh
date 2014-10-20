9f852beb16262e0ee6f1bd755bb6d5d77a689e4b
Emily Jane McTavish <ejmctavish@gmail.com>
2014-10-20 18:07:20 +0200

    assembla PLL environment

--- env.sh
+++ env.sh
@@ -1,2 +1,4 @@
-export PLL_HOME=~/projects/PLL-dev
-export LD_LIBRARY_PATH=~/projects/PLL-dev/lib
+#export PLL_HOME=~/projects/PLL-dev
+export PLL_HOME=~/projects/PLL-dev-assembla
+#export LD_LIBRARY_PATH=~/projects/PLL-dev/lib
+export LD_LIBRARY_PATH=~/projects/PLL-dev-assembla/lib

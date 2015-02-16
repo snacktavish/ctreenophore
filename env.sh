4194e09dbb43056b9deef21c5bb4fdcbe22378f3
Emily Jane McTavish <ejmctavish@gmail.com>
2015-02-16 15:06:54 +0100

    renamed

--- env.sh
+++
@@ -1,4 +0,0 @@
-#export PLL_HOME=~/projects/PLL-dev
-export PLL_HOME=~/projects/PLL-dev-assembla
-#export LD_LIBRARY_PATH=~/projects/PLL-dev/lib
-export LD_LIBRARY_PATH=~/projects/PLL-dev-assembla/lib
---
+++ updating/env.template.sh
@@ -0,0 +1,4 @@
+#export PLL_HOME=~/projects/PLL-dev
+export PLL_HOME=~/projects/pll
+#export LD_LIBRARY_PATH=~/projects/PLL-dev/lib
+export LD_LIBRARY_PATH=~/projects/pll
